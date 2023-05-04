f6c90df9d389ab13531173842f2ceb549ad28852
Jessé Lima <32416365+jflaviojf3@users.noreply.github.com>
2023-05-04 01:27:36 -0300

    inclusão de seeds

--- api/seeders/20230319041158-demo-tabelaGenerica.js
+++ api/seeders/20230319041158-demo-tabelaGenerica.js
@@ -19,8 +19,47 @@ module.exports = {
       descricaoCodigo: "Feminino",
       createdAt: new Date(),
       updatedAt: new Date()
+     },     
+     {
+      idPropriedade: 2,
+      descricaoPropriedade: "Perfil",
+      codPropriedade: 1,
+      descricaoCodigo: "Usuário",
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },     
+     {
+      idPropriedade: 2,
+      descricaoPropriedade: "Perfil",
+      codPropriedade: 2,
+      descricaoCodigo: "Funcionário",
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },     
+     {
+      idPropriedade: 2,
+      descricaoPropriedade: "Perfil",
+      codPropriedade: 3,
+      descricaoCodigo: "Gerente",
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },     
+     {
+      idPropriedade: 2,
+      descricaoPropriedade: "Perfil",
+      codPropriedade: 4,
+      descricaoCodigo: "Adm. Sistema",
+      createdAt: new Date(),
+      updatedAt: new Date()
+     },     
+     {
+      idPropriedade: 3,
+      descricaoPropriedade: "Categoria",
+      codPropriedade: 1,
+      descricaoCodigo: "Adm. Sistema",
+      createdAt: new Date(),
+      updatedAt: new Date()
      }], {});
-
   },
 
   async down (queryInterface, Sequelize) {
