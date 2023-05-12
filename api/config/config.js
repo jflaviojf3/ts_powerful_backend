dec5ce9b2e271f3419df4f80580534e8f5834575
jflaviojf3 <jflaviojf3@hotmail.com>
2023-05-11 22:30:40 -0300

    Não alterei nada de importante

--- api/config/config.js
+++ api/config/config.js
@@ -13,22 +13,31 @@ module.exports = {
       dateStrings: true,
       typeCast: true
     },
-
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    username: process.env.USERNAME_BANCO,
+    password: process.env.PASSWORD_BANCO,
+    database: process.env.DATABASE_BANCO,
+    host: process.env.HOST_BANCO,
+    port: process.env.PORT_BANCO,
+    dialect: process.env.DIALECT_BANCO,
+    timezone: "-03:00",
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true
+    },
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    username: process.env.USERNAME_BANCO,
+    password: process.env.PASSWORD_BANCO,
+    database: process.env.DATABASE_BANCO,
+    host: process.env.HOST_BANCO,
+    port: process.env.PORT_BANCO,
+    dialect: process.env.DIALECT_BANCO,
+    timezone: "-03:00",
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true
+    },
   },
 };
